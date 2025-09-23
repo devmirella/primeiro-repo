04d7095450f56157377308a829b38702150e2415
devmirella <marinasillva43@gmail.com>
2025-09-22 23:23:02 -0300

    ex5.py -> exercícios: Estrutura de Repetição

--- ex5.py
+++ ex5.py
@@ -38,31 +38,73 @@ for i in range(2,       numero + 1,   2): # começa em 2, vai até numero, pulan
 
 
 # ° Contar de 1 a 10:
-
 for i in range(1, 11):
     print(i)
 
 
 # ° Contar de 0 a 20 de 5 em 5
-
 for i in range(0, 21, 5):
     print(i)
 
 
 # ° Contar de tras pra frente de 10 a 1
-
 for i in range(10, 0 -1):
     print(i)
 
     
 # ° Fatorial 
+num = int(input("Digite um numero para calcular o fatorial: "))
+fatorial = 1
 
-numero = int(input("informe um numero para calcular o fatorial:"))
+for i in range(1, num +1):
+    fatorial *= i
+print(f"O fatorial de {num} é {fatorial}")
 
-if numero < 0:
-    print("Não é possível calcular fatorial de número negativo.")
-else:
-    fatorial = 1
-    for i in range(1, numero + 1):
-        fatorial *= i
-    print(f"O fatorial de {numero} é {fatorial}")
+
+# ° Media de Notas
+quantidade = int(input("Quantas notas você deseja digitar? "))
+soma = 0
+
+for i in range(quantidade):
+    nota = float(input(f"Digite a nota: {i + 1}"))
+    soma += nota 
+
+media = soma / quantidade
+print(f"A media é {media:.2f}")
+
+
+# ° Advinhação 
+import random
+
+segredo = random.randint(1, 10)
+tentativa = 0
+
+while True:
+    palpite = int(input("Advinhe um numero (ente 1 a 10) "))
+    tentaiva += 1
+
+    if palpite == segredo:
+        print(f"Parabéns, acertou em {tentaiva}")
+
+
+# ° Jogo de Advinhação
+
+    import random
+
+    segredo = random.randint(1, 10)
+    tentativa = 0
+
+    while True:
+        palpite = int(input("Adivinhe o número (1 a 10): ou digite 50 para sair."))
+        tentativa += 1
+
+        if palpite == segredo:
+            print(f"Parabéns! Você acertou {tentativa} tentativas ")
+            break 
+
+        elif palpite < segredo:
+            print(f"O número é maior.")
+    
+
+        else: 
+            print(f"O número é menor.")
