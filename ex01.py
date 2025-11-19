06569c00bc94f1e742f740d53a6b4bce0595f3c6
devmirella <marinasillva43@gmail.com>
2025-11-19 18:09:41 -0300

    -> exercício 1: imprime o básico

---
+++ ex01.py
@@ -0,0 +1,93 @@
+
+# EXERCÍCIO 1 - PRINTS BÁSICOS
+
+#PRIMEIRO EXERCICIO EM PYTHON
+print("OLÁ, MIRELLA!")
+print("Esse é o seu primeiro codigo em python.")
+print("Agora você esta oficialmente programando!")
+
+
+# EXERCÍCIO 2 - TABUADA DO 5 (USANDO PRINT)
+print("5 x 1 = 5 ")
+print("5 x 2 = 10")
+print("5 x 3 = 15 ")
+print("5 x 4 = 20")
+print("5 x 5 = 25 ")
+print("5 x 6 = 30 ")
+print("5 x 7 = 35 ")
+print("5 x 8 = 40 ")
+print("5 x 9 = 45 ")
+print("5 x 10 = 50 ")
+
+
+
+#EXERCÍCIO 3 - DESENEHE COM TEXTOS (USANDO PRINT)
+
+print("************")
+print("*          *")
+print("*          *")
+print("*          *")
+print("*          *")
+print("*          *")
+print("************")
+
+
+
+#EXERCÍCIO 4 - POEMA OU FRASE FAVORITO (USANDO PRINT)
+
+print("""
+Ainda bem que não morri de todas as vezes que quis, morrer
+que não soltei da ponte, nem enchi os pulsos de sangue
+nem me deitei ali lá longe, ainda bem que não atei a corda na viga do teto,
+nem comprei na farmácia,
+
+com receita fingida, uma dose de sono eterno,
+ainda bem que tive medo
+das facas, das alturas,
+e sobretudo de não morrer completamente,
+ficar por aí...
+
+ainda mais perdida do que antes,
+não olhar sem ver,
+ainda bem que o teto sempre foi demasiado alto
+e eu, ridiculamente pequena,
+
+pra morte.
+Se eu tivesse morrido de uma dessas vezes,
+não ouviria agora a sua voz a chamar
+enquanto escrevo esse poema, que pode não parecer,
+mas é um poema de amor.
+""")
+
+
+# EXERCÍCIO 4 - CONTINUAÇÃO
+
+print("                 EU SOU \n" 
+"          UNIVERSO DE ÁTOMOS, \n" 
+"             e ainda assim, \n" 
+"          UM ATOMO NO UNIVERSO.")
+
+# EXERCÍCIO 4 - CONTINUAÇÃO (USANDO PRINT COM CENTER)
+
+print("EU SOU".center(40))
+print("O UNIVERSO DE ÁTOMOS,".center(40))
+print("e ainda assim,".center(40))
+print("UM ÁTOMO NO UNIVERSO".center(40))
+
+
+# EXERCÍCIO 5 - CONTAR ATE 10 USANDO PRINT (SEM ESTRUTURAS DE REPETIÇÃO) 
+
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+print("FIM!")
+
+print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10 FIM!")
+
